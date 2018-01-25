71daa58f8440e0077a392464b5c68372ad0bfcd1
GrantK <grantklees108@gmail.com>
2018-01-25 13:43:47 -0800

    Changed to Period 5s Directions
    Changed To:
    1. Change the color of the arc at the top of the image and the oval to
    bright blue.
    2. Add an X that goes through the concentric circles. The X should cross
    at the center of the circles and should terminate at the edges of the
    circle.
    
    3. Add your name to the top of the image above the arc. The name needs
    to span the width of the image and should be centered. You must use a
    different font for your name.

--- 1.5.2.PY TkinterDraw.py
+++ 1.5.2.PY TkinterDraw.py
@@ -5,31 +5,28 @@ from Tkinter import *          #don't import like this except for Tkinter
 root = Tk()                      #create main window
 
 # Make and place a canvas widget for events and drawing
-canvas = Canvas(root, height=600, width=600, relief=RAISED, bg='white')
+canvas = Canvas(root, height=700, width=600, relief=RAISED, bg='white')
 canvas.grid() #Puts the canvas in the main Tk window
 
 # Make four objects on the canvas
-checkbox = canvas.create_rectangle(100, 200, 200, 300, dash=[1,4])
-check = canvas.create_line(100, 200, 200, 300, 150, 250, 100, 300, 200, 200,fill='red', width=20)
-message = canvas.create_text(380, 250, text='Try this!', font=('Arial', -100))
-myName = canvas.create_text(300, 500, text='Grant Klees', font=('Arial', -115))
-shadow = canvas.create_oval(100, 450, 500, 550, fill='#888888', outline='#888888')
+checkbox = canvas.create_rectangle(100, 300, 200, 400, dash=[1,4])
+x = canvas.create_line(270, 460, 330, 530, 300, 495, 270, 530, 330, 460,fill='red', width=20)
+check = canvas.create_line(100, 350, 150, 400, 215, 275, fill='red', width=20)
+message = canvas.create_text(380, 350, text='Try this!', font=('Arial', -100))
+myName = canvas.create_text(300, 60, text='Grant Klees', font=('Times New Roman', -115))
+shadow = canvas.create_oval(100, 550, 500, 650, fill='#00ccff', outline='#888888')
 canvas.tag_raise(myName, shadow)
 
 # Make an array of objects on the canvas
 circles=[]
 for r in range(10, 60, 10):
-    circles += [canvas.create_oval(300-r, 400-r, 300+r, 400+r, outline='red')]
+    circles += [canvas.create_oval(300-r, 500-r, 300+r, 500+r, outline='red')]
 
 # Make one more object on the canvas
-canopy = canvas.create_arc(0, 50, 600, 650, start=30, extent=120, width=50, style=ARC, outline = '#00ff00')          
+canopy = canvas.create_arc(0, 150, 600, 750, start=30, extent=120, width=50, style=ARC, outline = '#00ccff')          
 canvas.itemconfig(circles[0], fill = 'black', width = 5)
-e, f, g, h = canvas.coords(circles[0])
-canvas.coords(circles[0], e+5, f, g+5, h)
 # Demonstrate changing a property of canvas' item.
 canvas.itemconfig(circles[2], outline='black') # Change color
-a, b, c, d = canvas.coords(circles[2]) # Get coordinates
-canvas.coords(circles[2], a, b-5, c, d-5) # Change coordinates
 # Get a filename in the same directory as this program
 import os.path
 directory = os.path.dirname(os.path.abspath(__file__))
@@ -39,8 +36,8 @@ import PIL.Image, PIL.ImageTk
 img = PIL.Image.open(filename) # create a PIL.Image from the jpg fil
 tkimg = PIL.ImageTk.PhotoImage(img) # convert the PIL.Image to a PIL
 # Add the ImageTk object to the canvas.
-icon = canvas.create_image(150, 250, image = tkimg)
-canvas.tag_lower(icon, check)
+icon = canvas.create_image(150, 350, image = tkimg)
+canvas.tag_raise(icon, check)
 # Enter event loop. This displays the GUI and starts listening for events.
 # The program ends when you close the window.
 root.mainloop() 
